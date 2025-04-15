d66f3ded2853fa8c0c06d074caa4189fab9bd24e
adewaleijalana <adewaleijalana@gmail.com>
2025-04-15 23:01:08 +0100

    adding some comments

--- submission/04.sh
+++ submission/04.sh
@@ -3,6 +3,8 @@ publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 
 publicKeyHash=$(echo -n $publicKey | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160 -binary | xxd -p -c 32)
 
+#Using the 10_2_integer2lehex.sh script to convert the integer 1495584032 timestamp to little-endian hex gives 0420cd2459
+
 parts=("0420cd2459" "b1" "75" "76" "a9" "14" "$publicKeyHash" "88" "ac")
 
 redeem_script=""
--- submission/05.sh
+++ submission/05.sh
@@ -3,6 +3,7 @@ publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 
 publicKeyHash=$(echo -n $publicKey | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160 -binary | xxd -p -c 32)
 
+#Using the 10_2_integer2lehex.sh script to convert 150 blocks to little-endian hex gives 029600
 
 parts=("029600" "b2" "75" "76" "a9" "14" "$publicKeyHash" "88" "ac")
 
