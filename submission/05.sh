e120593ad0e1b7ad0cf31fbb5541db1db3c7258c
adewaleijalana <adewaleijalana@gmail.com>
2025-04-15 22:43:40 +0100

    creating CSV script

--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,14 @@
 # Create a CSV script that would lock funds until one hundred and fifty blocks had passed
-# publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+
+publicKeyHash=$(echo -n $publicKey | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160 -binary | xxd -p -c 32)
+
+
+parts=("029600" "b2" "75" "76" "a9" "14" "$publicKeyHash" "88" "ac")
+
+redeem_script=""
+for part in "${parts[@]}"; do
+  redeem_script+="$part"
+done
+
+echo $redeem_script
